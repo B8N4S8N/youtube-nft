8d0924aafb9dfbb9327252361ed459462a928f0d
B8N4S8N <theguttergod@guttermade.com>
2023-01-19 12:58:17 -0800

    adddddddddddddd

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import Profile from './profile.js';
+import UploadVideo from './UploadVideo';
 
 const Index = () => {
-  return <div>Hello, World!
-<button href="pages\profile.js">create profile</button>
-
-
-  </div>;
+    return (
+        <div>
+            <Profile />
+            <UploadVideo />
+        </div>
+    );
 };
 
 export default Index;
